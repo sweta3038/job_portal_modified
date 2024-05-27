e69e334286c46a3e8215b58d08ef96926a1efff5
sweta <swetasinha599@gmail.com>
2024-05-27 22:29:34 +0530

    first commit

--- part 9/frontend/src/redux/constants/jobconstant.js
+++
@@ -1,4 +0,0 @@
-export const JOB_LOAD_REQUEST = "JOB_LOAD_REQUEST";
-export const JOB_LOAD_SUCCESS = "JOB_LOAD_SUCCESS";
-export const JOB_LOAD_FAIL = "JOB_LOAD_FAIL";
-export const JOB_LOAD_RESET = "JOB_LOAD_RESET";
